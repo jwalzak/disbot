48400935a72063134b81f4f55ec15c2269007b37
Jason Walzak <walzak@gmail.com>
2021-11-17 13:54:03 -0500

    Added the /joke slash command. Changed the slash command from /user to /joke

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('interactionCreate', async interaction => {
   else if (commandName === 'server') {
     await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
   }
-   else if (commandName === 'user') {
+   else if (commandName === 'joke') {
     randomNum = joke[Math.floor(joke.length * Math.random())];
     await interaction.reply(`Here is your joke:\n ${randomNum}`);
   }
--- deploy-commands.js
+++ deploy-commands.js
@@ -13,6 +13,8 @@ const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('joke').setDescription('Replies with a corny joke.'),
+	
 ]
 	.map(command => command.toJSON());
 
