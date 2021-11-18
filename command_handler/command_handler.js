02dad01af43fa723422899b5ca7ab40767f1cea3
Jason Walzak <walzak@gmail.com>
2021-11-18 11:54:00 -0500

    Breaking up the code for better reading Making the joke list a better organised JSON file.

---
+++ command_handler/command_handler.js
@@ -0,0 +1,15 @@
+require('dotenv');
+const { REST } = require('@discord=js/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+const token = process.env.DISCORD_TOKEN;
+
+const commands = [];
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error);
